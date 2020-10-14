fba8eb699140efa5100aa51e74fc8e982220199d
andresperezfc <felipe3apc@gmail.com>
2020-10-14 00:56:41 -0500

    Primer Commit

---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,44 @@
+import 'package:estados/bloc/usuario/usuario_bloc.dart';
+import 'package:estados/models/usuario.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Pagina2Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina 2'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MaterialButton(
+              child: Text('Establecer Usuario',
+                  style: TextStyle(color: Colors.white)),
+              color: Colors.blue,
+              onPressed: () {
+                final newUser = new Usuario(
+                  nombre: 'Andrés Pérez',
+                  edad: 25,
+                  profesiones: ['Desarrollador de software'],
+                );
+                BlocProvider.of<UsuarioBloc>(context)
+                    .add(ActivarUsuario(newUser));
+              }),
+          MaterialButton(
+              child:
+                  Text('Cambiar Edad', style: TextStyle(color: Colors.white)),
+              color: Colors.blue,
+              onPressed: () {}),
+          MaterialButton(
+              child: Text('Añadir Profesion',
+                  style: TextStyle(color: Colors.white)),
+              color: Colors.blue,
+              onPressed: () {}),
+        ],
+      )),
+    );
+  }
+}
